a5a706e0f56e380be79364c7e58fd7ef550f50db
lpeti69 <loveipetya@gmail.com>
2018-11-22 21:58:34 +0100

    Added functions for RL

--- framework/pacmanAgents.py
+++ framework/pacmanAgents.py
@@ -92,7 +92,63 @@ class ReinforcementLearningAgent(Agent):
     def chooseAction(self, gameState):
         util.raiseNotDefined()
 
-class MyAgent(Agent): ## TODO
+class MyAgent(ReinforcementLearningAgent): ## TODO
+
+    def registerInitialState(self, gameState):
+        self.start = gameState.getAgentPosition(self.index)
+        ReinforcementLearningAgent.registerInitialState(self, gameState)
+
+    def chooseAction(self, gameState):
+        actions = gameState.getLegalActions(self.index)
+
+        # You can profile your evaluation time by uncommenting these lines
+        # start = time.time()
+        values = [self.evaluate(gameState, a) for a in actions]
+        # print 'eval time for agent %d: %.4f' % (self.index, time.time() - start)
+
+        maxValue = max(values)
+        bestActions = [a for a, v in zip(actions, values) if v == maxValue]
+
+        foodLeft = len(self.getFood(gameState).asList())
+
+        if foodLeft <= 2:
+        bestDist = 9999
+        for action in actions:
+            successor = self.getSuccessor(gameState, action)
+            pos2 = successor.getAgentPosition(self.index)
+            dist = self.getMazeDistance(self.start,pos2)
+            if dist < bestDist:
+            bestAction = action
+            bestDist = dist
+        return bestAction
+
+        return random.choice(bestActions)
+
+    def getSuccessor(self, gameState, action):
+        successor = gameState.generateSuccessor(self.index, action)
+        pos = successor.getAgentState(self.index).getPosition()
+        if pos != nearestPoint(pos):
+        # Only half a grid position was covered
+        return successor.generateSuccessor(self.index, action)
+        else:
+        return successor
+
+    def evaluate(self, gameState, action):
+        features = self.getFeatures(gameState, action)
+        weights = self.getWeights(gameState, action)
+        return features * weights
+
+    def getFeatures(self, gameState, action):
+        features = util.Counter()
+        successor = self.getSuccessor(gameState, action)
+        ## TODO
+        for feature in [lambda state: 0]:
+            features[feature] = feature(successor)
+        return features
+
+    def getWeights(self, gameState, action):
+        return {'asd': 1.0}
+
     def getAction(self, state):
         return Directions.STOP
 
