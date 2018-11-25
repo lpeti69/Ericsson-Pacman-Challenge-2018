c36e7eb90822d05a3d0d158ed9da7e2ccd4a7bcb
lpeti69 <loveipetya@gmail.com>
2018-11-25 14:18:36 +0100

    Reloaded getDir

--- final.py
+++ final.py
@@ -409,7 +409,12 @@ class Game:
         P.y, P.x = npos
         if nfld == '.': P.points += 10
         elif nfld == 'o': P.points += 50
-<<<<<<< HEAD
+    
+    def out(self, a1, a2=''):
+        a1 = self.getDir(a1)
+        a2 = self.getDir(a2) if a2!='' else ''
+        sys.stdout.write("%s %s %s %s\n" % (G.id, G.tick, G.getOwn().id, a1+a2))
+        sys.stderr.write("%s %s %s %s\n" % (G.id, G.tick, G.getOwn().id, a1+a2))
 
     def getDir(self, d):
         if d == (0,1):
@@ -421,14 +426,6 @@ class Game:
         if d == (-1,0):
             return '^'
         return "ERROR DIR"
-=======
-    
-    def out(self, a1, a2=''):
-        a1 = self.getDir(a1)
-        a2 = self.getDir(a2) if a2!='' else ''
-        sys.stdout.write("%s %s %s %s\n" % (G.id, G.tick, G.getOwn().id, a1+a2))
-        sys.stderr.write("%s %s %s %s\n" % (G.id, G.tick, G.getOwn().id, a1+a2))
->>>>>>> 41dea65fe008d85a6681636479134e171469abd2
         
     
     def _readline(self):
@@ -483,16 +480,6 @@ while G.read():
     sys.stderr.write("%d, %d\n" % (G.M.width, G.M.height))
     sys.stderr.write("%s\n" % G.getClosests((17,13)))
     
-<<<<<<< HEAD
-    actions = ''
-    action = G.agent.getPolicy(G)
-    actions += G.getDir(action)
-    if G.getOwn().getBoosterRemain() > 0:
-        G.update(action)
-        action = G.agent.getPolicy(G)
-        actions += G.getDir(action)
-    sys.stdout.write("") ## TODO
-=======
     a1 = G.agent.getPolicy(G)
     #sys.stdout.write("%s" % G.getDir(action))
     #sys.stderr.write("%s" % G.M)
@@ -502,6 +489,5 @@ while G.read():
         G.out(a1, a2)
     else:
         G.out(a1)
->>>>>>> 41dea65fe008d85a6681636479134e171469abd2
     #  c o d e   g o e s   h e r e
     
